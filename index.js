073115d3edd3d88f25549513b8c831a880b7456c
Matthew Guzman <mattguzmanedu@gmail.com>
2022-05-11 11:46:58 -0600

    redid index.js and added more comments

--- index.js
+++ index.js
@@ -1,87 +1,148 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// Global require's
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./src/generateMarkdown.js')
 
-function askQuestions() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "What is your user GitHub username?",
-            name: "username"
-        },
-        {
-            type: "input",
-            message: "What is your email?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is URL of your project in GitHub?",
-            name: "url"
-        },
-        {
-            type: "input",
-            message: "What is the title of your project?",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "Please provide a description of your project.",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "What packages need to be installed to run your project.",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "What technologies were used to create your project.",
-            name: "technology"
-        },
-        {
-            type: "input",
-            message: "Please provide an example of how your project can be used.",
-            name: "usage"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "What kind of license would you like to have?",
-            name: "license",
-            choices: ["MIT", "ISC", "Perl"]
-        },
-        {
-            type: "input",
-            message: "Including yourself, please list out all contributors",
-            name: "contributer"
-        },
-        {
-            type: "input",
-            message: "What command is used to run a test",
-            name: "tests",
+// Array of questions to ask the user
+const questions = [
+    // Project name
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }
         }
-    ]);
-}
+    },
+    // Description of project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to provide a project description!');
+                return false;
+            }
+        }
+    },
+    // Installation Instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project? (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue!');
+                return false;
+            }
+        }
+    },
+    // Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use project!');
+                return false;
+            }
+        }
+    },
+    // Contribution Guidlines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this project? (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the project!');
+                return false;
+            }
+        }
+    },
+    // Test Instructions 
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project? (Required)',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this project!');
+                return false;
+            }
+        }
+    },
+    // License Options
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project (Required)',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project!');
+                return false;
+            }
+        }
+    },
+    // Github Username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Would you like to include your email?',
+    },
+];
 
-function WriteToFile(fileName, data) {
-    fs.writeFile(fileName, data, "utf8", function (err) {
-        if (err) {
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
             throw err;
-        }
-        console.log("You have successfully written your README file");
+        console.log('Success! Information transferred to the README!')
     });
-}
+};
 
-async function init() {
-    try {
-        const answers = await askQuestions();
-        generateMarkdown(answers);
-        WriteToFile("README.md", generateMarkdown(answers));
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
-    } catch (err) {
-        console.log(err);
-    }
-}
-
+// Function call to initialize app
 init();
